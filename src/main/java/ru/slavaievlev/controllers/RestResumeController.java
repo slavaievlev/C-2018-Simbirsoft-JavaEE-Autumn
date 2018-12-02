220c55a55302bf1e831d6f8fc7508389e63456ca
Слава Иевлев <slavaievlev9@gmail.com>
2018-12-02 13:31:45 +0400

    Added RestController

---
+++ src/main/java/ru/slavaievlev/controllers/RestResumeController.java
@@ -0,0 +1,10 @@
+package ru.slavaievlev.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/resume")
+public class RestResumeController {
+
+}
